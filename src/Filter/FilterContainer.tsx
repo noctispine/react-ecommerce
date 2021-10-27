4263ad2e082727d6cd44a1de983fe14ce74ee213
noctispine <pinefromw@gmail.com>
2021-10-27 16:34:42 +0300

    init filter component

---
+++ src/Filter/FilterContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const FilterContainer = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FilterContainer
---
+++ src/Filter/filter.styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const FilterContainer = styled.div`
+  width: 100%;
+`
