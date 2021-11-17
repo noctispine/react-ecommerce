184cc0c54cd16076410de12fc13debc944c8ee52
noctispine <pinefromw@gmail.com>
2021-11-17 14:36:31 +0300

    add fetchProductsByCategoriesWatcher

--- src/sagas/rootSaga.ts
+++ src/sagas/rootSaga.ts
@@ -1,11 +1,12 @@
 import { all, fork } from '@redux-saga/core/effects'
-import fetchProducts from './productSaga'
+import {fetchProducts, fetchProductsByCategories} from './productSaga'
 import { loginWatcher, registerationWatcher } from './authenticationSaga'
 import { addToCartWatcher, removeFromCartWatcher, fetchCartWatcher } from './cartSaga'
 
 export default function* rootSaga() {
   yield all([
     fork(fetchProducts),
+    fork(fetchProductsByCategories),
     fork(loginWatcher),
     fork(registerationWatcher),
     fork(addToCartWatcher),
